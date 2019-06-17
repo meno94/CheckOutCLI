0ade33e2c8820fa6df92f7c79d0f3e9c891ad360
Milen Pishimarov <mgpishimarov@gmail.com>
2019-06-18 02:39:08 +0300

    component 2

--- product.js
+++ product.js
@@ -7,32 +7,68 @@ const orange = {
     price: 0.25
 }
 
+const appleDiscountActive = true;
+const orangeDiscountActive = true;
+
 function buildArray(cart) {
     return cart.split(',');
+}
+
+
+
+function getCounts(cart) {
+
+    var counts = {};
+    cart.forEach(function (x) {
+        counts[x] = (counts[x] || 0) + 1;
+    });
+
+    return counts;
 }
 
 function calculatePrice(cart) {
     fruitList = buildArray(cart);
 
+    var fruitCount = getCounts(fruitList);
+
+    var appleDiscount = 0;
+    var orangeDiscount = 0;
+    if (Math.floor(fruitCount['apple'] / 2) != NaN) {
+        appleDiscount = Math.floor(fruitCount['apple'] / 2) * apple.price;
+    }
+
+    if (Math.floor(fruitCount['orange'] / 3)) {
+        orangeDiscount = Math.floor(fruitCount['orange'] / 3) * orange.price;
+    }
+
+
+
     var totalAmount = 0;
 
     fruitList.forEach(fruit => {
-
-
-
-
-
-
-
 
         switch (fruit) {
             case 'orange':
                 totalAmount += orange.price;
                 break;
             case 'apple':
-                totalAmount += apple.price;
+                if (appleDiscountActive) {
+                    totalAmount += apple.price;
+                }
+                break;
+            default:
+                break;
         }
     });
+
+
+    if (appleDiscount != NaN) {
+        totalAmount = totalAmount - appleDiscount;
+    }
+
+    if (orangeDiscount != NaN) {
+        totalAmount = totalAmount - orangeDiscount;
+    }
 
     return totalAmount;
 }
